491a662ce4f3622db8731de48622896c6f05c720
Zhaoyikaiii <stryhytrgb@gmail.com>
2025-09-25 17:49:44 +0800

    feat: unify-quyery 日志以及报错可读性修改 #1010158081127701392

--- pkg/unify-query/service/http/query_test.go
+++ pkg/unify-query/service/http/query_test.go
@@ -26,6 +26,7 @@ import (
 
 	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/unify-query/bkapi"
 	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/unify-query/consul"
+	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/unify-query/errno"
 	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/unify-query/featureFlag"
 	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/unify-query/influxdb"
 	"github.com/TencentBlueKing/bkmonitor-datalink/pkg/unify-query/influxdb/decoder"
@@ -251,7 +252,12 @@ func TestQueryTsWithEs(t *testing.T) {
 
 			res, err := queryTsWithPromEngine(ctx, c.queryTs)
 			if err != nil {
-				log.Errorf(ctx, err.Error())
+				codedErr := errno.ErrBusinessQueryExecution().
+					WithComponent("HTTP查询测试").
+					WithOperation("执行查询测试").
+					WithContext("error", err.Error()).
+					WithSolution("检查测试查询配置和数据")
+				log.ErrorWithCodef(ctx, codedErr)
 				return
 			}
 			data := res.(*PromData)
@@ -1329,7 +1335,7 @@ func TestQueryTs(t *testing.T) {
 			assert.Nil(t, err)
 			actual := string(out)
 			fmt.Printf("ActualResult: %v\n", actual)
-			log.Infof(t.Context(), "ActualResult: %v", actual)
+			log.Infof(context.Background(), "ActualResult: %v", actual)
 			assert.Equal(t, c.result, actual)
 		})
 	}
